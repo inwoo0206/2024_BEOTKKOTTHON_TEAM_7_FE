26b8ef9104119134ff9f5d58a73ea5fd99b9aaee
조인식 <imsif42@naver.com>
2024-03-14 19:14:02 +0900

    Feat: 메인페이지 welcome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import MainCarousel from '../components/Home/MainCarousel';
+import Welcome from '../components/Home/Welcome';
 
 export default function Home() {
   return (
     <div>
       <MainCarousel />
+      <div style={{ height: '10px' }}></div>
+      <Welcome />
     </div>
   );
 }
