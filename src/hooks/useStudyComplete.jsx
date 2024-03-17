18147b72954798d02526ef92e32293a4cfaf0fce
조인식 <imsif42@naver.com>
2024-03-17 18:16:56 +0900

    Fix: hook 경로 수정

--- src/hooks/useStudyComplete.jsx
+++ src/hooks/useStudyComplete.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { api } from '../../utils/customAxios';
+import { api } from '../utils/customAxios';
 
 const getCompletedStudy = async () => {
   const res = await api.get('/study/complete');
