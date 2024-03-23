0ae4a4eeac5df6d019a9edfb44d745566fe5fc2c
권인우 <138741672+inwoo0206@users.noreply.github.com>
2024-03-23 19:37:30 +0900

    pull success

--- src/hooks/useHome.jsx
+++ src/hooks/useHome.jsx
@@ -19,15 +19,6 @@ const popularStudy = async () => {
   }
 };
 
-const popularStudy = async () => {
-  try {
-    const data = await api.get("/study/comment3");
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 export const useHome = () => {
   const { data: best } = useQuery({
     queryKey: ["BestStudy"],
